20b431607b76a57105694c4a510d9bb3bbfbfdbb
Enaium <wyy-666@hotmail.com>
2024-03-27 21:56:49 +0800

    update to 1.6

---
+++ KeyBindUI/Framework/Gui/ModsGui.cs
@@ -0,0 +1,50 @@
+using EnaiumToolKit.Framework.Screen;
+using EnaiumToolKit.Framework.Screen.Elements;
+using KeyBindUI.Framework.Entity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using StardewModdingAPI.Utilities;
+
+namespace KeyBindUI.Framework.Gui;
+
+public class KeyBindGui : ScreenGui
+{
+    public KeyBindGui() : base("Key Bind UI")
+    {
+        var mods = Environment.CurrentDirectory + "/" + "Mods";
+        foreach (var info in new DirectoryInfo(mods).GetDirectories())
+        {
+            var keyBindListInfo = new KeyBindListInfo();
+            var config = new FileInfo(info.FullName + "/config.json");
+            keyBindListInfo.ConfigFileInfo = config;
+            if (!config.Exists)
+            {
+                continue;
+            }
+
+            keyBindListInfo.Name = info.Name;
+            var read = new JsonTextReader(config.OpenText());
+            var readFrom = (JObject) JToken.ReadFrom(read);
+            read.Close();
+            foreach (var keyValuePair in readFrom)
+            {
+                if (keyValuePair.Value is not { Type: JTokenType.String }) continue;
+                try
+                {
+                    keyBindListInfo.KeyBindList[keyValuePair.Key] =
+                        KeybindList.Parse(keyValuePair.Value.ToString()).ToString();
+                }
+                catch (Exception e)
+                {
+                    // ignored
+                }
+            }
+
+            AddElement(new Button(info.Name,
+                ModEntry.GetInstance().Helper.Translation.Get("KeyBindUI.Framework.Screen.ModsGui.KeySetting"))
+            {
+                OnLeftClicked = () => { OpenScreenGui(new KeyBindListGui(keyBindListInfo)); }
+            });
+        }
+    }
+}
